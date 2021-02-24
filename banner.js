f4725b59b7c533c6fc5bb78efea0a4b4421335bd
Ying Zhou <yingzhou@thoughtworks.com>
2021-02-24 10:00:10 +0800

    v1.1

--- banner.js
+++ banner.js
@@ -1,22 +1,18 @@
-var keyName = 'danger-banner-domain-list';
-chrome.storage.local.get({ [keyName]: [] }, function (result) {
-  if (result[keyName].includes(window.location.hostname)) {
-    var banner = document.createElement('div');
+const keyName = 'danger-banner-path-list';
+chrome.storage.local.get({ [keyName]: [] }, (result) => {
+  if (result[keyName].some((path) => window.location.href.match(path))) {
+    const banner = document.createElement('div');
     banner.id = 'danger-banner';
     banner.style = 'position: fixed;' +
       'top: 0;' +
+      'left: 0;' +
       'width: 100vw;' +
-      'text-align: center;' +
-      'background-color: #EE312D;' +
-      'color: white;' +
-      'font-size: 16px;' +
-      'font-family: sans-serif;' +
-      'font-weight: bold;' +
-      'line-height: 40px;' +
-      'height: 40px;' +
+      'height: 100vh;' +
+      'box-sizing: border-box;' +
+      'border: solid #EE312D 3px;' +
+      'background-image: repeating-linear-gradient(-30deg, #EE312D10, #EE312D10 50px, transparent 50px, transparent 100px);' +
+      'pointer-events: none;' +
       'z-index: 9999999';
-    banner.appendChild(document.createTextNode('DANGEROUS AREA'));
     document.body.appendChild(banner);
-    document.body.style = 'border-top: solid #EE312D 40px;';
   }
 });
--- options.js
+++ options.js
@@ -1,50 +1,42 @@
-function initialize() {
-  var keyName = 'danger-banner-domain-list';
-  var dangerousDomains;
+const initialize = () => {
+  const keyName = 'danger-banner-path-list';
   document.getElementById('container').remove();
-  var container = document.createElement('div');
+  const container = document.createElement('div');
   container.id = 'container';
   document.body.insertBefore(container, document.getElementById('buttons'));
-  chrome.storage.local.get({ [keyName]: [] }, function (result) {
+  chrome.storage.local.get({ [keyName]: [] }, (result) => {
     if (result[keyName].length === 0) {
       addRow(undefined, '');
-      return;
-    }
-    for (var i = 0; i < result[keyName].length; i++) {
-      addRow(undefined, result[keyName][i]);
+    } else {
+      result[keyName].forEach((path) => {
+        addRow(undefined, path);
+      });
     }
   });
-}
+};
 
-function saveOptions() {
-  var keyName = 'danger-banner-domain-list';
-  var domains = document.getElementsByTagName('input');
-  var dangerousDomains = [];
-  for (var i = 0; i < domains.length; i++) {
-    var currentDomain = domains[i].value.trim();
-    if (currentDomain !== '') {
-      currentDomain = currentDomain.replace('http://', '');
-      currentDomain = currentDomain.replace('https://', '');
-      currentDomain = currentDomain.replace(/\/.*/, '');
-      if (!dangerousDomains.includes(currentDomain)) {
-        dangerousDomains.push(currentDomain);
-      }
+const saveOptions = () => {
+  const keyName = 'danger-banner-path-list';
+  const paths = document.getElementsByTagName('input');
+  const dangerousPaths = [];
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i].value.trim();
+    if (path !== '' && !dangerousPaths.includes(path)) {
+      dangerousPaths.push(path);
     }
   }
-  chrome.storage.local.set({ [keyName]: dangerousDomains }, function () {
+  chrome.storage.local.set({ [keyName]: dangerousPaths }, function () {
     console.log('Saved');
   });
   initialize();
-}
+};
 
-function addRow(event, initValue) {
-  var newRow = document.createElement('input');
-  newRow.placeholder = 'Please input dangerous domain';
-  if (initValue) {
-    newRow.value = initValue;
-  }
+const addRow = (event, initValue) => {
+  const newRow = document.createElement('input');
+  newRow.placeholder = 'Please input dangerous path';
+  newRow.value = initValue || '';
   document.getElementById('container').appendChild(newRow);
-}
+};
 
 document.getElementById('save').addEventListener('click', saveOptions);
 document.getElementById('add').addEventListener('click', addRow);
